b0e91fc969b4f2622671311cb114d59b31d5978a
Charles Butler <chuck@dasroot.net>
2016-02-29 16:05:47 -0500

    checking it in

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Layer Logstash
 
 This is a charm layer for logstash.
+
+
+### This charm is not stand-alone
+
+This charm makes use of `interface:java` which means you will need to deploy
+a compatible JRE along with the logstash service. This allows the consumer to
+swap the version of java being used by configuring the system's java installation.
+
+     juju deploy logstash
+     juju deploy openjdk
+     juju add-relation logstash openjdk
+ 
--- reactive/logstash.py
+++ reactive/logstash.py
@@ -5,8 +5,15 @@ from charmhelpers.core.fetch import configure_sources
 from charmhelpers.core.fetch import apt_install
 from charmhelpers.core.fetch import apt_update
 from charmhelpers.core.hookenv import config
+from charmhelpers.core.hookenv import status_set 
 
 
+@when_not('java.ready')
+def messaging():
+    status_set('blocked', 'Missing JRE')
+
+# this is declared in the JRE provider. 
+@when('java.ready')
 @when_not('logstash.installed')
 def fetch_and_install():
     configure_sources(config('apt-url'), config('apt-key'))
